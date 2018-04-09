02bcf9d1403e5161829bb5f749872acc6a8fa253
sklee <hadesleeq@gmail.com>
2018-04-09 17:53:06 +0900

    Printing Some Numbers

---
+++ src/Introduction/PrintingSomeNumbers2.scala
@@ -0,0 +1,12 @@
+package Introduction
+
+object PrintingSomeNumbers2 {
+  def main(args: Array[String]): Unit = {
+    for {
+      i <- 1 to 10
+       j <- 1 to 10
+    }
+      println(i * j)
+  }
+
+}
