f3a0aa958907069bf1ea3b281f3691d1d6687155
sklee <hadesleeq@gmail.com>
2018-04-11 11:22:27 +0900

    The following code defines a Person class with immutable first and last names, but a mutable age.

--- src/Introduction/Mutablevariable.scala
+++ src/Introduction/Mutablevariable.scala
@@ -1,11 +1,13 @@
-package Introduction
+class Person(val name: String, var age: Int)
 
 object Mutablevariable {
   def main(args: Array[String]): Unit = {
 
-    var stockPrice: Double = 100.0
-    stockPrice = 200.0
-    println(stockPrice)
+    val p = new Person("Dean Wampler", 29)
+    println(p.name)
+    println(p.age)
+    p.age = 30
+    println(p.age)
   }
 
 }
