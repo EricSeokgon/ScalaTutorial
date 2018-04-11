785f0bff250e35c72b27dc93691c942f1a110a4c
sklee <hadesleeq@gmail.com>
2018-04-11 11:18:20 +0900

    Array elements themselves are mutable, so the elements can be modified:

---
+++ src/Introduction/VariableDeclarations.scala
@@ -0,0 +1,12 @@
+package Introduction
+
+object VariableDeclarations {
+  def main(args: Array[String]) {
+
+    var array: Array[String] = new Array(5)
+    array = new Array(2)
+    array(0) = "Hello"
+    println(array)
+
+  }
+}
