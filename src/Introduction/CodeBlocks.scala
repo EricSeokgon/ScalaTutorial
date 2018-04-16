1fdd732485ce988d4eda7b8402665aa9d8656e32
sklee <hadesleeq@gmail.com>
2018-04-16 13:54:11 +0900

    Scala Code Blocks

---
+++ src/Introduction/CodeBlocks.scala
@@ -0,0 +1,23 @@
+package Introduction
+
+object CodeBlocks {
+  def meth1(): String = {
+    "hi"
+  }
+
+  def meth2(): String = {
+    val d = new java.util.Date()
+    d.toString
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(meth1())
+    println(meth2())
+  }
+
+  val x3: String = {
+    val d = new java.util.Date()
+    d.toString
+  }
+
+}
