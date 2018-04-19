a235817c6851aadf31733eb24a01ed40c4423f64
sklee <hadesleeq@gmail.com>
2018-04-19 14:43:41 +0900

    Scala Tuples

---
+++ src/DataType/Tuples.scala
@@ -0,0 +1,12 @@
+package DataType
+
+object Tuples {
+  def main(args: Array[String]): Unit = {
+    val tuple1 = (1, false, "Scala")
+    val tuple2 = "title" -> "Beginning Scala"
+    val tuple = (1, false, "Scala")
+    val third = tuple._3
+
+  }
+
+}
